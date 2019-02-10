412ab26b3aa4279cde050f58cee448cd63d9d4a4
zcp <is99zcp@cqut.edu.cn>
2019-02-10 12:10:34 -0500

    a commit

--- test.sh
+++ test.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-pid=$1
-      while : 
-      do
-          #echo  $result-thread-count-$filter_keyword
-          timestamp=`date +"%Y-%m-%d,%H:%M:%S.%3N"`
-          thread_count=$(cat /proc/$pid/status | grep Threads)
-          nice -n 1 echo $timestamp, $thread_count  >> test.txt
-          #ps -o nlwp $container_pid
-          # >> $result-thread-count-$filter_keyword
-          sleep 1
-          #echo \#`date +"%Y-%m-%d-%H:%M:%S.%3N"` >> $result 
-      done
 
+mysql_conf=/home/zcp/mysqld.cnf
+sed -i '/^bind_address=.*/a skip-grant-tables' $mysql_conf
+sed -i "s/^bind_address=.*/#bind_address=/" $mysql_conf
